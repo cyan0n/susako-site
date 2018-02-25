3e26c34d4c05518f463dd6188c8d1876082dc4b8
Thomas Cipriani <thomas.cipriani@esimple.it>
2018-02-25 14:47:51 +0100

    Base Model/Controller/Migrations

---
+++ database/migrations/2018_02_25_130935_create_pieces_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePiecesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pieces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->nullable()->unsigned();
+            $table->string('name');
+            $table->string('url_name');
+            $table->string('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pieces');
+    }
+}
