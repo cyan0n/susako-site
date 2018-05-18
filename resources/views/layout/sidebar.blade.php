35441960658d8b9acc551d0b640dfef17b4d26d0
cyan0n <cyan0n.dev@gmail.com>
2018-05-18 18:23:44 +0200

    Added logo to sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,13 +1,16 @@
-<aside id="sidebar" class="menu">
-	<ul class="menu-list">
-		<li>
-			<a href="{{ route('home') }}">Home</a>
-			@foreach ($main_categories as $category)
-				<a href="/{{ $category->url_name }}">{{ $category->name }}</a>
-			@endforeach
-			<a href="/{{ route('about') }}">About</a>
-			<a href="/{{ route('commission') }}">Commission</a>
-			<a href="/{{ route('contact') }}">Contact</a>
-		</li>
-	</ul>
+<aside id="sidebar">
+	<img id="logo" src="{{ asset('images/logo.png') }}">
+	<div class="menu">
+		<ul class="menu-list">
+			<li>
+				<a href="{{ route('home') }}">Home</a>
+				@foreach ($main_categories as $category)
+					<a href="/{{ $category->url_name }}">{{ $category->name }}</a>
+				@endforeach
+				<a href="/{{ route('about') }}">About</a>
+				<a href="/{{ route('commission') }}">Commission</a>
+				<a href="/{{ route('contact') }}">Contact</a>
+			</li>
+		</ul>
+	</div>
 </aside>
