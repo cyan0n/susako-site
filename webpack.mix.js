436794a811026bf7e9aa80fc3f57680d8dfd2074
Thomas Cipriani <thomas.cipriani@esimple.it>
2018-06-20 12:34:48 +0200

    Added SourceMaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,5 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/admin.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/admin.scss', 'public/css')
+    .sourceMaps()
     .copyDirectory('resources/assets/images', 'public/images');
