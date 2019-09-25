4ba1995cfbd64301007e765643ec908c8683bbf6
Cyan0n <cyan0n.dev@gmail.com>
2019-09-25 16:55:53 +0200

    Typo fix

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Susanna Montania'),
+    'name' => env('APP_NAME', 'Susanna Montinia'),
 
     /*
     |--------------------------------------------------------------------------
